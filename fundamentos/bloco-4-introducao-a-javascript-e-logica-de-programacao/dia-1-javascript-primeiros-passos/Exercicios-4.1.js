f1a2676c200f6429d8aa4558fa34cc45593e1f6a
BrenoBTrindade <brenokaminski@hotmail.com>
2022-02-02 20:01:48 -0300

    resolve os exercicios 1-3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicios-4.1.js
@@ -0,0 +1,50 @@
+/*
+// Exercicio 1
+const a = 20;
+const b = 30;
+// Adição
+console.log(a+b);
+
+//Subtração
+console.log(a-b);
+
+//Multiplicação
+console.log(a*b);
+
+//Divisão
+console.log(a/b);
+
+//Módulo
+console.log(b%a);
+
+
+// Exercicio 2
+const num1 = 10;
+const num2 = 15;
+
+if( num1>num2){
+  console.log(num1);
+}
+else if( num2>num1){
+  console.log(num2);
+}
+*/
+
+// Exercicio3
+
+const num1 = 30;
+const num2 = 20;
+const num3 = 15;
+
+if( num1 > num2 && num1 > num3 ){
+  console.log(num1);
+}
+else if(num2 > num1 && num2 > num3){
+  console.log(num2);
+}
+else if(num3 > num2 && num3 > num1){
+  console.log(num3);
+}
+
+
+
