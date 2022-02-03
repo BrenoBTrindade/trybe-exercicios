2b3dfd7839ac0e36b20c3a8bbc909aafa899b890
BrenoBTrindade <brenokaminski@hotmail.com>
2022-02-03 15:52:44 -0300

    arquivo scripts.js do dia criado com exercicios do conteudo resolvidos.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,138 @@
+// Exercicios Conteudo
+// Array
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length);
+// 3
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+// Brincar com o cachorro
+
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+tasksList.unshift('acordar');// adiciona uma tarefa no inicio do array
+console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+tasksList.pop();  // remove a última tarefa (.shift apaga a primeira tarefa)
+console.log(tasksList);
+// [ 'fazer exercicios da trybe' ]
+
+let indexOftask = tasksList.indexOf('Reunião');
+console.log(indexOftask);
+
+//Exercicio Array.
+
+//Exercicio 1.
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+// [ 'serviços´]
+
+//Exercicio 2.
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+// 2
+
+//Exercicio 3.
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push('Contato')
+console.log(menu);
+// [ 'Home', 'Serviços', 'Portfólio', 'Links', 'Contato' ]
+
+// Exercicios For
+
+//Exericicio Video
+var numero =9;
+
+for(var contador =1; contador <=9 ; contador++){
+
+  console.log(numero * contador);
+  //9 18 27 36 45 54 63 72 81
+}
+
+//Exercicio for e array;
+var listaDeNomes = ['joana', 'Maria',  'lucas'];
+
+for(var indice = 0; indice < listaDeNomes.length; indice += 1) {
+  var mensagem = 'Boas vindas, ' + listaDeNomes[indice] + '!';
+  console.log(mensagem);
+}
+
+//Exercicio 2
+let cars = ["Saab", "Volvo", "BMW"];
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+  // saab
+  //Volvo
+  // BMW
+}
+
+// exercicio 3
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index =0; index < groceryList.length; index += 1){
+  console.log(groceryList[index]);
+}
+
+// Ecercicios For/of
+
+let numeros = [1,2,3,4,5];
+for(let numero of numeros) {
+  console.log(numero);
+}
+
+// resultado: 
+//1
+//2
+//3
+//4
+//5
+
+
+let word = 'Hello';
+for (let palavras of word) {
+  console.log(palavras);
+}
+// resultado:
+// "H"
+// "e"
+// "l"
+// "l"
+// "o"
+
+//Soma Valores
+let arrOfNumbers = [10, 20, 30];
+for (let sum of arrOfNumbers) {
+  sum += 1;
+  console.log(sum);
+}
+// 11
+// 21
+// 31
+console.log(arrOfNumbers);
+// Resultado: [10, 20, 30]
+
+//Exercicio 1
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let nomes of names){
+  console.log(nomes);
+  // Resultado: João
+  //Maria
+  //Antônio
+  //Margarida
+
+}
+//
