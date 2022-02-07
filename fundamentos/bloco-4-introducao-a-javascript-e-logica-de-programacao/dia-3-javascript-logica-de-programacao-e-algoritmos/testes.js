c79007abd280258de6c6afa8b6003b1db3151680
BrenoBTrindade <brenokaminski@hotmail.com>
2022-02-07 12:34:44 -0300

    resolve exercicis 1 e 2 do dia 4.3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/testes.js
@@ -0,0 +1,140 @@
+let array= [];
+
+for(let i =1; i <=25; i +=1){
+  array.push(i)
+}
+  for (let index = 0; index < array.length; index += 1) {
+    console.log(array[index] / 2);
+  }
+
+console.log(array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let resultado = 0;
+
+
+
+// let menorValor = 0;
+// menorValor = numbers[0]; 
+
+// for(let i=0; i < numbers.length; i += 1){
+//   if ( numbers[i] < menorValor){
+//     menorValor=numbers[i]
+//   }
+
+// }
+// console.log(menorValor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i=0; i < numbers.length; i += 1){
+//   if(numbers[i]/2 !==1 ){
+//     resultado = resultado +1;
+//   }
+// }
+//   if(resultado === 0){
+//     console.log('nenhum impar')
+//   }else{
+//     console.log('valores impares:',resultado);
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let maiorValor = 0;
+// maiorValor = numbers[0]; 
+
+// for(let i=0; i < numbers.length; i += 1){
+//   if ( numbers[i] > maiorValor){
+//     maiorValor=numbers[i]
+//   }
+
+// }
+// console.log(maiorValor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(i = 0; i < numbers.length; i += 1){
+//   aritmetico += numbers[i]/10;
+// }
+
+// console.log(aritmetico);
+// if(aritmetico>20){
+//   console.log('valor maior que 20');
+// }else{
+//   console.log('valor menos ou igual a 20');
+// }
