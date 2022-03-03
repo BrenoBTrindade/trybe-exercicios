e9ae6d3d47e8ed9ada74163b6659a54604d09634
BrenoBTrindade <brenokaminski@hotmail.com>
2022-03-03 20:09:33 -0300

    exercicios do dia

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/Exercicios.js
@@ -0,0 +1,39 @@
+// const testingScope = (escopo) => {
+//   if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+ 
+// }
+
+// testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const sorted = oddsAndEvens.sort((a,b) => {
+  if(a < b) return -1;
+  if(a > b) return 1;
+  return 0;
+});
+
+
+console.log(sorted); 
+
+//Parte 2!
+
+const fatorial = (x) => {
+  if (x ==0){
+    return 1;
+  } else {
+    return x * fatorial(x - 1);
+  }
+}
+
+let num = 0;
+ if(num > 0){
+   let resultado = fatorial(num);
+   console.log(resultado);
+ }
